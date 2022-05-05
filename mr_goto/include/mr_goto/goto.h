405fc0384e59ca0eb63f535dd5c637404df15b70
Georg Amstetter <georg.amstetter@gmail.com>
2022-05-05 22:43:27 +0200

    added new node for Goto exercise with a subscription to move_base_simple/goal

---
+++ mr_goto/include/mr_goto/goto.h
@@ -0,0 +1,60 @@
+#ifndef GOTO_H
+#define GOTO_H
+
+#include <vector>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <mr_geometry/geometry.h>
+#include <mr_goto/GotoConfig.h>
+
+namespace moro {
+/**
+ * Robot class
+ */
+class Goto {
+public:
+    enum ControlMode {
+        STOP = 0,
+        BUG1 = 1,
+        BUG2 = 2
+    };
+    enum ActionState {
+        NA   = 0,    /// init
+        INIT = 1,    /// init
+        WAIT = 2,    /// wait for new commands 
+        TURN = 3,     /// turn to goal
+        STRAIGHT = 4, 
+        WALL_FOLLOW_LEFT = 5, 
+        WALL_FOLLOW_RIGHT = 6 
+        /// @ToDo for goto expand action state if needed
+    };
+    static std::map<ControlMode, std::string> ControlModeName_; 
+    
+    
+    Goto(const std::string &ns); /// Constructor
+    void init();                         /// initialization
+    void ai();                           /// artificial intelligence calls a behavior
+    void plot();                         /// plots sensor input
+    
+protected:
+
+    Command cmd_;  /// output variables  v, w
+    unsigned long loop_count_; /// counts the filter cycles
+    Pose2D goal_;  /// goal pose in world coordinates
+    Pose2D start_; /// start pose in world coordinates
+    Pose2D odom_;  /// current pose based on odometrie
+    ActionState action_state_;  /// current action state
+
+    MeasurementLaser measurement_laser_;    /// laser measurements
+
+    Figure figure_local_;  /// Figure for data visualization
+
+    void bug1();            /// Bug1 behavior
+    void bug2();            /// Bug2 behavior
+    void plotLocal();       /// plots sensor input in robot coordinates
+    mr_goto::GotoConfig config_;
+};
+}
+
+#endif // GOTO_H
+
---
+++ mr_goto/src/goto_node.cpp
@@ -0,0 +1,135 @@
+#include "goto_node.h"
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <tf/transform_datatypes.h>
+
+using namespace moro;
+int main ( int argc, char **argv ) {
+
+    ros::init ( argc, argv, "goto" );  /// initializes the ros node with default name
+    ros::NodeHandle n;
+    GotoNode planner ( n );
+    planner.init();
+    ros::Rate rate ( 10 );  /// ros loop frequency synchronized with the wall time (simulated time)
+
+    while ( ros::ok() ) {
+
+        /// calls your loop
+        planner.ai();
+
+        /// sets and publishes velocity commands
+        planner.publishMotion();
+
+        /// plots measurements
+        planner.plot();
+
+        /// calls all callbacks waiting in the queue
+        ros::spinOnce();
+
+        /// sleeps for the time remaining to let us hit our publish rate
+        rate.sleep();
+    }
+    return 0;
+}
+
+/**
+ * Constructor
+ **/
+GotoNode::GotoNode ( ros::NodeHandle & n )
+    : Goto ( ros::NodeHandle ( "~" ).getNamespace() ),
+      n_ ( n ),
+      n_param_ ( "~" ) {
+
+     sub_laser_ = n.subscribe("scan", 1000, &GotoNode::callbackLaser, this);
+
+
+    /**
+     * @ToDo GoTo
+     * subscribes the fnc callbackGoal to goal and fnc callbackOdometry to odom
+     **/
+#if PLANNER_EXERCISE >= 10
+#else
+    /**
+     * @node your code
+     **/
+    // sub_goal_ =
+     sub_goal_ = n.subscribe("move_base_simple/goal", 1000, &GotoNode::callbackGoal, this);
+    // sub_odom_ =
+#endif
+
+    /// defines a publisher for velocity commands
+    pub_cmd_ = n.advertise<geometry_msgs::Twist> ( "cmd_vel", 1 );
+
+    reconfigureFnc_ = boost::bind ( &GotoNode::callbackConfigGoto, this,  _1, _2 );
+    reconfigureServer_.setCallback ( reconfigureFnc_ );
+}
+
+void GotoNode::callbackConfigGoto ( mr_goto::GotoConfig &config, uint32_t level ) {
+    ROS_INFO ( "callbackConfigGoto!" );
+    config_ = config;
+    init();
+}
+
+/**
+ * copies incoming laser messages to the base class
+ * @param laser
+ **/
+void GotoNode::callbackLaser ( const sensor_msgs::LaserScan &_laser ) {
+
+    measurement_laser_.range_max() = _laser.range_max;
+    measurement_laser_.range_min() = _laser.range_min;
+    measurement_laser_.resize ( _laser.ranges.size() );
+    for ( int i = 0; i < measurement_laser_.size(); i++ ) {
+     measurement_laser_[i].length  = _laser.ranges[i];
+     measurement_laser_[i].angle  = _laser.angle_min +_laser.angle_increment*i;
+     measurement_laser_[i].end_point  = Point2D(0.22+measurement_laser_[i].length * cos(measurement_laser_[i].angle), measurement_laser_[i].length * sin(measurement_laser_[i].angle));
+    }
+}
+/**
+ * copies incoming odometry messages to the base class
+ * @param odom
+ **/
+void GotoNode::callbackOdometry ( const nav_msgs::Odometry &odom ) {
+    tf::Quaternion q;
+    tf::quaternionMsgToTF ( odom.pose.pose.orientation, q );
+    double roll = 0, pitch = 0, yaw = 0;
+    tf::Matrix3x3 ( q ).getRPY ( roll, pitch, yaw );
+    double a = yaw;
+    odom_.set ( odom.pose.pose.position.x, odom.pose.pose.position.y, a );
+    odom_.recompute_cached_cos_sin();
+    ROS_INFO ( "callbackOdomGoto!" );
+}
+
+
+/**
+ * copies incoming pose messages to the base class
+ * @param odom
+ **/
+void GotoNode::callbackGoal ( const geometry_msgs::PoseStamped& goal ) {
+    tf::Quaternion q;
+    tf::quaternionMsgToTF ( goal.pose.orientation, q );
+    double roll = 0, pitch = 0, yaw = 0;
+    tf::Matrix3x3 ( q ).getRPY ( roll, pitch, yaw );
+    goal_.set ( goal.pose.position.x, goal.pose.position.y, yaw );
+    goal_.recompute_cached_cos_sin();
+
+    start_ = odom_;
+    action_state_ = ActionState::INIT;
+
+    Point2D goal_local;
+    ROS_INFO ( "goal received! %4.3f,%4.3f and %3.2f rad",  goal_.x(), goal_.y(), goal_.theta() );
+}
+
+
+/**
+ * Publishes motion commands for a robot
+ **/
+void GotoNode::publishMotion () {
+    geometry_msgs::Twist cmd;
+    /// creates motion command
+    cmd.linear.x = cmd_.v();
+    cmd.linear.y = 0.;
+    cmd.angular.z = cmd_.w();
+    /// publishes motion command
+    pub_cmd_.publish ( cmd );
+}
