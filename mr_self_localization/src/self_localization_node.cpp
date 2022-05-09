ea71b0eda52b1ada8247ecdc89c7b9d19cb09bb2
Clemens <clemens@ubuntu>
2022-05-09 15:18:15 +0200

    fixed map rotation

--- mr_self_localization/src/self_localization_node.cpp
+++ mr_self_localization/src/self_localization_node.cpp
@@ -289,7 +289,7 @@ void SelfLocalizationNode::publishMap() {
     std::vector<int8_t> data(width * height, -1);
     for (int x = 0; x < width; ++x) {
         for (int y = 0; y < height; ++y) {
-            data[x * width + y] = (255 - background.at<uint8_t>(x, width - y)) * 100 / 255;
+            data[x * width + y] = (255 - background.at<uint8_t>(height - x, y)) * 100 / 255;
         }
     }
 
@@ -307,3 +307,17 @@ void SelfLocalizationNode::publishMap() {
 
     pub_map.publish(map);
 }
+
+
+/*local planer subscriber der auf debug message hört
+
+
+
+
+
+
+
+
+
+
+*/
