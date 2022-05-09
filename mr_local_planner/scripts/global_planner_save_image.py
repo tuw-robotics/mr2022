bce1dddffd2fc9a1d1635cefc2e3888c2cb95342
Andreas B <andreas.brandstaetter@tuwien.ac.at>
2022-05-09 15:46:06 +0200

    first working global planner

--- mr_local_planner/scripts/global_planner_save_image.py
+++ mr_local_planner/scripts/global_planner_save_image.py
@@ -32,7 +32,7 @@ class Map:
 
 class GlobalPlanner(object):
     def __init__(self):
-        rospy.loginfo("GlobalPlanner init")
+        rospy.loginfo("GlobalPlanner: init")
         
         rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.goal_callback, queue_size = 1)
         rospy.Subscriber('/initialpose', PoseWithCovarianceStamped, self.initialpose_callback, queue_size = 1)
@@ -45,48 +45,28 @@ class GlobalPlanner(object):
         self.initpose.pose.pose.position.x = 0
         self.initpose.pose.pose.position.y = 0
 
-        self.N_STEPS = 5
-
-        self.export_images = True
-        self.export_debug = True
-        self.verbose = True
-        self.occupied_thresh = 0.5 # TODO get from map meta infos
-
-        #self.output_path = "/home/andreas/PHD/Mobile_Robotics/workspace_goto/maplogs/debug"
-        self.output_path = ""
+        self.export_debug_images = True
+        self.output_path = "/tmp/ros_global_planner_debug"
         self.output_filename_root, _ = os.path.splitext(self.output_path)
 
-        #image_filename = os.path.join(self.output_path, "/home/andreas/PHD/Mobile_Robotics/workspace_goto/maplogs/line.png")
-        image_filename = os.path.join(self.output_path, "~/Uni_TInf/mobile_robotics/catkin_group/map_images/line.png")
-        self.image = io.imread(image_filename, as_gray=True).astype(np.float)
-        self.normalized_image = self.image / np.amax(self.image.flatten()) # normalize image
-        self.binary_image = self.normalized_image > (self.occupied_thresh)
-        
+        self.occupied_thresh = 0.5 # this is normalized (0 to 1)
+
+        self.erosion_value = 18 #12
+        self.d_value = 25
+        self.use_blurred_factor = True
+        self.sample_every = 30 #20
+
+        self.race_line_points = np.zeros(2);
         self.grid_starting_position = np.zeros(2);
         self.world_starting_position = np.zeros(2);
         self.grid_goal_position = np.zeros(2);
         self.world_goal_position = np.zeros(2);
-        self.resolution = 0.032; 
-        self.origin = np.array([-8.0, -8.0]);
-        self.image_shape = np.array([558, 558]);
-        self.image_center = (self.image_shape + self.origin / self.resolution).astype(np.int)
+        self.resolution = 1; 
+        self.origin = np.array([0.0, 0.0]);
+        self.image_shape = np.array([10, 10]);
         self.map_init = False
         self.map = None
 
-        self.erosion_value = 12
-        self.d_value = 25
-        self.use_blurred_factor = True
-        self.sample_every = 15
-
-        if self.export_debug:
-            rospy.loginfo("Verbose infos:")
-            rospy.loginfo("Center position (grid coordinates): [x,y] = [{:.2f}, {:.2f}]".format(
-                self.image_center[0], self.image_center[1]
-            ))
-            rospy.loginfo("Occupied threshold: {:.4f}".format(self.occupied_thresh))
-            rospy.loginfo("Min. pixel value*: {:8.4f} (*) before normalization".format(np.amin(self.image)))
-            rospy.loginfo("Max. pixel value*: {:8.4f}".format(np.amax(self.image)))
-            #print("Dimensions: {:d} x {:d}".format(self.image.shape[0], self.image.shape[1]))
 
     # ===================================================================================================================
     def compute_distance_transform(self, starting_position, goal_position, erosion_value = 0, forward_direction = True):
@@ -109,8 +89,8 @@ class GlobalPlanner(object):
         mask_start[goal_position[1], goal_position[0]+2] = True
         mask_target[goal_position[1], goal_position[0]-2] = True
 
-        rospy.loginfo("Computing distance to goal [{}, {}] from position [{}, {}] ({} direction)...".format(
-            goal_position[0], goal_position[1], starting_position[0], starting_position[1], "forward" if forward_direction else "backward"))
+        rospy.loginfo("Computing distance to goal [{}, {}] from position [{}, {}] ({} direction with erosion {})...".format(
+            goal_position[0], goal_position[1], starting_position[0], starting_position[1], "forward" if forward_direction else "backward", erosion_value))
 
         current_distance = 0.0
         while True:
@@ -122,8 +102,8 @@ class GlobalPlanner(object):
                 break
             distances[x,y] = current_distance
             mask = np.logical_or(mask, new_pixels)
-            if self.verbose and (np.mod(current_distance, 100) == 0):
-                rospy.loginfo("distance = {}".format(current_distance))
+            #if(np.mod(current_distance, 100) == 0):
+            #    rospy.loginfo("distance = {}".format(current_distance))
 
         distances[np.nonzero(finish_line)] = (current_distance)
         distances = distances * self.resolution
@@ -131,7 +111,7 @@ class GlobalPlanner(object):
         drivable_area = np.logical_or(mask, finish_line)
 
         #For debugging:
-        if self.export_debug:
+        if self.export_debug_images:
             io.imsave(self.output_filename_root + '.' + filename_extra + '.binary_image.png', img_as_ubyte(binary_image), check_contrast=False)
             io.imsave(self.output_filename_root + '.' + filename_extra + '.normalized_distances.png', img_as_ubyte(normalized_distances), check_contrast=False)
 
@@ -176,26 +156,21 @@ class GlobalPlanner(object):
         distances_blurred_border_start_blank = ndimage.gaussian_filter(distances_extended_start_blank, sigma=5) * drivable_area
         distances_blurred_border = distances_blurred_border_target_blank * mask_start + distances_blurred_border_start_blank * (1-mask_start)
 
-        #if erosion_value == 0:
-        #    distances = distances + distances_blurred * 0.04 + distances_blurred_border * 0.02
-        #else:
-        #    distances = distances + distances_blurred * 0.08 + distances_blurred_border * 0.06
-
         if self.use_blurred_factor:
-            distances = distances + distances_blurred * 0.08 + distances_blurred_border * 0.06
+            distances = distances + distances_blurred * 0.1 + distances_blurred_border * 0.1
 
         distances = distances * self.resolution
         normalized_distances = distances / np.amax(distances.flatten())
 
         #For debugging:
-        if self.export_debug:
+        if self.export_debug_images:
             io.imsave(self.output_filename_root + '.normalized_distances_blurred.png', img_as_ubyte(normalized_distances), check_contrast=False)
 
         return drivable_area, distances, normalized_distances, mask_start, mask_start_small, mask_target, mask_target_small
 
     # ===================================================================================================================
 
-    def compute_raceline(self, starting_position, current_drivable_area, full_drivable_area, normalized_target_distances, filename_part):
+    def compute_raceline(self, starting_position, goal_position, current_drivable_area, full_drivable_area, normalized_target_distances, filename_part):
         race_line = np.zeros_like(self.binary_image, np.float)
         spline_line = np.zeros_like(self.binary_image, np.float)
 
@@ -222,11 +197,10 @@ class GlobalPlanner(object):
 #            if np.mod(a_distance,15) == 0:
             if np.mod(a_distance,self.sample_every) == 0:
                 cv = np.append(cv, [race_pos], axis=0)
-                rospy.loginfo("Add path point: {} {}".format(race_pos[0], race_pos[1]))
+                rospy.logdebug("Add path point: {} {}".format(race_pos[0], race_pos[1]))
                 
-
             #For debugging:
-            #if self.export_debug:
+            #if self.export_debug_images:
             #    print("  best_pos {} {} val {} from current val {}".format(best_pos[0], best_pos[1], best_val, normalized_target_distances[race_pos[0], race_pos[1]]))
 
             race_pos = best_pos
@@ -236,38 +210,30 @@ class GlobalPlanner(object):
             race_line[race_pos[0], race_pos[1]] = 1;
 
             if np.mod(a_distance,50) == 0:
-                rospy.loginfo("a_distance: {}".format(a_distance))
+                rospy.logdebug("a_distance: {}".format(a_distance))
             if a_distance >= 5000:
                 break
 
+        cv = np.append(cv, [[goal_position[1], goal_position[0]]], axis=0)
+        self.race_line_points = cv;
+
         #For debugging:
-        if self.export_debug:
+        if self.export_debug_images:
             race_line = morphology.binary_dilation(race_line, selem=morphology.selem.square(width=2, dtype=np.bool)) # dilate for thicker line
             io.imsave(self.output_filename_root + '.race_line_' + filename_part + '.png', img_as_ubyte(race_line * 255) - self.binary_image.astype(float) * 150)
 
+        # optional: use spline interpolation
         #d = 35
-        
-        d = self.d_value
+        #d = self.d_value
         #p = si.BSpline(cv,1000,d,True)
         #p = scipy_bspline(cv,n=1000,degree=d,periodic=True)
         
-        #cv_save = (cv - 1000) * self.map_properties['resolution']
-        #cv_save[:,[0, 1]] = cv_save[:,[1, 0]]
-        #cv_save = cv_save - 1000 #np.array(self.map_properties['origin'])[0:2]
-        #p_save = (p - 1000) * self.map_properties['resolution']
-        #p_save[:,[0, 1]] = p_save[:,[1, 0]]
-        #p_save[:,1] = 0 - p_save[:,1] # invert y axis
-        #np.array(self.map_properties['origin'])[0:2]
-        
-        #np.savetxt(self.output_filename_root + '.spline_line_cv.csv', cv_save, delimiter=",")
-        #np.savetxt(self.output_filename_root + '.spline_line_p.csv', p_save, delimiter=",")
-
         spline_line = race_line
         #for [spline_x, spline_y] in p.astype(int):
         #    spline_line[spline_x, spline_y] = 1;
 
         #For debugging:
-        if self.export_debug:
+        if self.export_debug_images:
             spline_line = morphology.binary_dilation(spline_line, selem=morphology.selem.square(width=2, dtype=np.bool)) # dilate for thicker line
             io.imsave(self.output_filename_root + '.spline_line.png', img_as_ubyte(spline_line * 255) - self.binary_image.astype(float) * 150)
 
@@ -283,9 +249,11 @@ class GlobalPlanner(object):
 
     def goal_callback(self, goal_msg):
         if(self.initpose.header.frame_id == "unknown"):
-            rospy.loginfo("inital pose was not set before")
+            rospy.loginfo("GlobalPlanner: inital pose was not set before")
+        elif not self.map_init:
+            rospy.loginfo("GlobalPlanner: map not yet received")
         else:
-            rospy.loginfo("goal_callback received")
+            rospy.loginfo("GlobalPlanner: goal_callback received")
             
             self.world_starting_position[0] = self.initpose.pose.pose.position.x
             self.world_starting_position[1] = self.initpose.pose.pose.position.y
@@ -324,38 +292,32 @@ class GlobalPlanner(object):
             #drivable_area, distance_from_start_line, normalized_distance_from_start_line, _, _, _, _ = \
             #    self.compute_distance_transform(starting_position=self.grid_starting_position, goal_position=self.grid_goal_position, forward_direction = True)
 
-            self.compute_raceline(self.grid_starting_position, drivable_area, drivable_area, distance_to_target_smoothed, "full")
-            normalized_spline_line = self.compute_raceline(self.grid_starting_position, drivable_area_eroded, drivable_area, distance_to_target_eroded_smoothed, "eroded")
-            
-            
+            self.compute_raceline(self.grid_starting_position, self.grid_goal_position, drivable_area, drivable_area, distance_to_target_smoothed, "full")
+            normalized_spline_line = self.compute_raceline(self.grid_starting_position, self.grid_goal_position, drivable_area_eroded, drivable_area, distance_to_target_eroded_smoothed, "eroded")
+
             msg = Path()
             msg.header.frame_id = "map"
             msg.header.stamp = rospy.get_rostime()
-            for i in range(self.N_STEPS):
+            for line_point in self.race_line_points:
+                world_line_point = line_point * self.resolution + self.origin
+
                 pose = PoseStamped()
-                pose.pose.position.x = self.initpose.pose.pose.position.x + (goal_msg.pose.position.x - self.initpose.pose.pose.position.x)/(self.N_STEPS-1)*i
-                pose.pose.position.y = self.initpose.pose.pose.position.y + (goal_msg.pose.position.y - self.initpose.pose.pose.position.y)/(self.N_STEPS-1)*i
+                pose.pose.position.x = world_line_point[1]
+                pose.pose.position.y = -world_line_point[0]
                 pose.pose.position.z = 0
-                if(i == 0):
-                    pose.pose.orientation = self.initpose.pose.pose.orientation
-                if(i >= self.N_STEPS-1):
-                    pose.pose.orientation = goal_msg.pose.orientation
-                #quaternion = tf.transformations.quaternion_from_euler(0, 0, 0)
-                #pose.pose.orientation.x = quaternion[0]
-                #pose.pose.orientation.y = quaternion[1]
-                #pose.pose.orientation.z = quaternion[2]
-                #pose.pose.orientation.w = quaternion[3]
+                pose.pose.orientation = goal_msg.pose.orientation # only applied by local planner for target position.
                 msg.poses.append(pose)
             
             self.path_publisher.publish(msg)
+ 
 
     def initialpose_callback(self, pose_msg):
-        rospy.loginfo("initialpose_callback received")
+        rospy.loginfo("GlobalPlanner: initialpose_callback received")
         self.initpose = pose_msg
         #rospy.loginfo(self.initpose)
 
     def map_callback(self, map_msg):
-        rospy.loginfo("map_callback received")
+        #rospy.loginfo("GlobalPlanner: map_callback received")
         if not self.map_init:
             res = map_msg.info.resolution
             width = map_msg.info.width
@@ -375,6 +337,14 @@ class GlobalPlanner(object):
             
             self.map = Map(width, height, res, map_data, [origin_x, origin_y])
             self.map_init = True
+
+            self.image = np.flip(map_data.astype(np.float), 0)
+            self.normalized_image = self.image / np.amax(self.image.flatten()) # normalize image
+            self.binary_image = self.normalized_image < (self.occupied_thresh)
+            
+            rospy.loginfo("Min. pixel value*: {:8.4f} (*) before normalization".format(np.amin(self.image)))
+            rospy.loginfo("Max. pixel value*: {:8.4f}".format(np.amax(self.image)))
+                        
 
             # map_uint8 = self.map.data.astype(np.uint8)
             # map_uint8 = map_uint8 * (255/100)
