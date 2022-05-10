d892a00583b815fc1185d0acffca065fa3ddb8b0
11811341 <e11811341@student.tuwien.ac.at>
2022-05-10 21:15:04 +0200

    Task4: fixed basic goto

--- mr_local_planner/src/mr_local_planner/local_planner.cpp
+++ mr_local_planner/src/mr_local_planner/local_planner.cpp
@@ -23,6 +23,10 @@ LocalPlanner::LocalPlanner ( const std::string &ns )
 }
 
 void LocalPlanner::init() {
+    
+    wait_count_ = 0;
+    waiting_ = false;
+    prev_goal_ = Pose2D(0, 0, 0);
 
     figure_local_.init ( config_.map_pix_x, config_.map_pix_y,
                          config_.map_min_x, config_.map_max_x,
@@ -218,22 +222,46 @@ void LocalPlanner::bug1() {
     if (measurement_laser_.empty() || (goal_.x()==0 && goal_.y()==0)) {
         v = 0.0, w = 0.0;
     } else {
-        std::cout<<odom_.theta()<<std::endl;
-        std::cout<<goal_.theta()<<std::endl;
-        std::cout<<"------------"<<std::endl;
         
-        if(odom_.theta()!=goal_.theta() && (((odom_.theta()<goal_.theta())&&(goal_.theta()-odom_.theta()>1))||((odom_.theta()>goal_.theta())&&(odom_.theta()-goal_.theta()>1)))){
-            if(odom_.theta()<goal_.theta())
-                w=1.0;
-            else
-                w=-1.0;
-
-        }else{
-            v = 2.0;
-            w = 0.0;
+        cv::Vec2d robot_pointing(cos(odom_.theta()), sin(odom_.theta()));
+        cv::Vec2d odom2goal(goal_.x()-odom_.x(), goal_.y()-odom_.y());
+        
+        double dot = robot_pointing(0) * odom2goal(0) + robot_pointing(1) * odom2goal(1);
+        double det = robot_pointing(0) * odom2goal(1) - robot_pointing(1) * odom2goal(0);
+        double angle = atan2(det, dot);
+        
+        //target angle for the robot to turn to
+        double target_theta = odom_.theta()+angle;
+        
+       
+        
+        //check if a new goal has been set, reset driving if yes
+        if(prev_goal_.x()!=goal_.x() && prev_goal_.y()!=goal_.y()){
+            waiting_ = false;
+            wait_count_ = 0;
         }
+        prev_goal_ = goal_;
         
-        
+        //check if rotation has been completed and if the timer to counteract oversteering has run down
+        if(waiting_ && wait_count_==10){
+            double dist = sqrt(pow(odom2goal(0),2)+pow(odom2goal(1),2));
+            v = 1.0;
+            if(dist<1){
+               v = 0.0;
+            }
+        }else if(waiting_){
+            wait_count_++;
+        }else{
+            //if the rotation is within a tolerance, stop turning
+            if(abs(target_theta-odom_.theta())<0.5){
+                waiting_ = true;
+            }else{
+                if(target_theta>odom_.theta())
+                    w = 1.0;
+                else if(target_theta<odom_.theta())
+                    w = -1.0;
+            }
+        }
     }
     
     cmd_.set(v,w);
